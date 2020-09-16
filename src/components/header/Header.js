c51dd898b90ae11b5fb5d5f8b2484f30f1d67558
maryadipoipo187@gmail.com <maryadipoipo187@gmail.com>
2020-09-16 11:29:58 +0700

    add Header to App.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Header() {
     return(
-        <nav class="navbar navbar-dark bg-primary">
+        <nav class="navbar navbar-dark bg-info">
             <div className="row col-12 d-flex justify-content-center text-white">
             <span className="h3">Register</span>
             </div>
